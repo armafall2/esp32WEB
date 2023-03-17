0f8fadee2e3cef96fa00290fdbc88d559791deef
armafall2 <mat.lur518@gmail.com>
2023-03-17 10:28:33 +0100

    test Mongo DB

---
+++ api/createCollection.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require('mongodb');
+
+const uri = "mongodb+srv://ArmaFall:80msln02L@esp32web.z6xixsl.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function createCollection() {
+    try {
+        await client.connect();
+        const database = client.db("test");
+        const collection = database.collection("Test");
+        const result = await collection.insertOne({ "oui": 3 });
+        console.log(`${result.insertedCount} documents were inserted into the collection.`);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+module.exports = async (req, res) => {
+    await createCollection();
+    res.status(200).send('Collection créée avec succès !');
+};
