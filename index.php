fd136d738060ff0299443ad812b5b283f3318ccc
Mathis Lurin <mathis.lur1@gmail.com>
2023-03-17 08:53:34 +0100

    Premier Dépot : Ajout Index

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+echo "le php fonctionne :D";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<p>Le html Fonctionne</p>
+</body>
+<footer>
+
+</footer>
+</html>
